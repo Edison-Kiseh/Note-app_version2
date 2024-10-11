90e25d1401dc02159aebe0e123553546a8294559
edison <eddytiger7@gmail.com>
2024-10-11 13:27:37 +0200

    Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { NotebooksComponent } from './notebooks/notebooks.component';
+import { HomeComponent } from './home/home.component';
+import { BinComponent } from './bin/bin.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { HomeNotebooksComponent } from './home-notebooks/home-notebooks.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NotebooksComponent, HomeComponent, BinComponent, RouterModule, NavigationComponent, HomeNotebooksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
